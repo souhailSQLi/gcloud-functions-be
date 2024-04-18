81e3f8c784aeedce31b7003849f24ee6611e291e
Souhail RAZZOUK <srazzouk+gh@sqli.com>
2024-04-18 08:30:23 +0100

    Init repo with running code and config with debugger

---
+++ fun-1/src/index.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "@google-cloud/functions-framework";
+
+// Register an HTTP function with the Functions Framework that will be executed
+// when you make an HTTP request to the deployed function's endpoint.
+export const helloTSHttp = (req: Request, res: Response) => {
+  const { headers } = req;
+  if (headers.accept === "application/json") {
+    console.log("yo yo yo yo");
+    return res.json({ message: "Hello World from TS Function 1" });
+  }
+  return res.send("Hello World from TS Function 1");
+};
+
